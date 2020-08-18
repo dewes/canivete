b930ef6232fbd076f6de0c111af50b0ec6377752
Dewes <rdewes@gmail.com>
2020-08-18 17:07:02 -0300

    Adicionado configuração para CORS

--- app/main.py
+++ app/main.py
@@ -1,11 +1,31 @@
+import os
 from typing import Optional
 
 from fastapi import Depends, FastAPI, Header, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
 from .routers import textos
 
 app = FastAPI()
 
+# Configuração para permitir execução de JS remoto
+origins = [
+    'http://localhost', 'https://localhost',
+    os.environ.get('cors_host', '')
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+# Apps #
+
+# Textos #
 app.include_router(
     textos.router,
     prefix="/texto",
@@ -16,6 +36,7 @@ app.include_router(
 )
 
 
+# Raiz #
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
